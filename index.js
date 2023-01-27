c88aee7a5327a929d1b353ceda50b7aa7ba649dd
Maxim Lyashenko <lyashenkokhpi2018@gmail.com>
2023-01-27 19:34:37 +0200

    done

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const server = http.createServer();
-server.on('request', async (req, res) => {
-    try {
-        const url = req.url.slice(1);
-        fs.rm(url, { recursive: true }, (error) => {
-            console.log(error);
-        })
-        
-        if (!fs.existsSync(url)) {
-            console.log(200);
-        } else if (!!fs.existsSync(url)) {
-            throw new Error('cant file')
-        }
-
-        res.end()
-    } catch (error) {
-        if (error.message === 'cant file') {
-            res.end('404')
-        }
-        console.log(error.message);
-    }
-
+const server = require('./server');
+server.listen(3000, () => {
+    console.log('Server is listening on http://localhost:3000');
 })
-
-server.listen(3000)
